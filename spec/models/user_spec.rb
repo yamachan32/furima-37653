9481781f1c4232cfdfe211e57a0eca6ed7e71aee
yamachan32 <apropiado.para.m@gmail.com>
2022-03-12 22:02:03 +0900

    Issue4 ユーザー管理機能 レビュー内容の反映
    - Fakerのpassword生成ルールの修正
    - ビューファイルのコメントアウト削除
    - Userモデルの単体テストパターンの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe User, type: :model do
       it "全ての情報が正しく入力されると新規登録できる" do
         expect(@user).to be_valid
       end
-      
+
     end
     
     context '新規登録できないとき' do
@@ -57,8 +57,15 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
       end
 
-      it "パスワードは、半角英数字混合での入力でないと登録できない" do
+      it "パスワードは、半角数字のみでは登録できない" do
         @user.password = "123456"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password  is invalid. Include both letters and numbers")
+      end
+
+      it "パスワードは、半角英数字のみでは登録できない" do
+        @user.password = "password"
         @user.password_confirmation = @user.password
         @user.valid?
         expect(@user.errors.full_messages).to include("Password  is invalid. Include both letters and numbers")
@@ -68,6 +75,13 @@ RSpec.describe User, type: :model do
         @user.password = "password2"
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+      it "パスワードは、全角での入力では登録できない" do
+        @user.password = "ｐ１２３４５"
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password  is invalid. Include both letters and numbers")
       end
 
       it "苗字が空白だと登録できない" do
@@ -112,12 +126,24 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include("Last name ruby  Input full-width katakana characters")
       end
       
+      it "苗字カナが全角でもカタカナ以外だと登録できない" do
+        @user.last_name_ruby = "みょうじ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name ruby  Input full-width katakana characters")
+      end
+
       it "名前カナが半角だと登録できない" do
         @user.first_name_ruby = "ﾅﾏｴ"
         @user.valid?
         expect(@user.errors.full_messages).to include("First name ruby  Input full-width katakana characters")
       end
       
+      it "名前カナが全角でもカタカナ以外だと登録できない" do
+        @user.first_name_ruby = "なまえ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name ruby  Input full-width katakana characters")
+      end
+
       it "生年月日が空白だと登録できない" do
         @user.birthday = ""
         @user.valid?
