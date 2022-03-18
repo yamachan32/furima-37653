ceab17b0ff0bbba6b6a15cb0eba2bedbf02bde7d
yamachan32 <apropiado.para.m@gmail.com>
2022-03-18 15:31:41 +0900

    Issue10 商品購入機能 単体テストコードの追加
    - order_addressモデルのFactoryBotを設定
    - order_addressモデルのテストコードを作成

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,90 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+    before do
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id:user.id, item_id:item.id)
+      sleep(0.5)
+    end
+
+    describe '商品購入' do
+      context '商品購入できるとき' do
+        it '全ての情報が正しく入力されると購入できる' do
+          expect(@order_address).to be_valid
+        end
+        it '建物名の情報が空欄でも購入できる' do
+          @order_address.building = ''
+          expect(@order_address).to be_valid
+        end
+      end
+
+      context '商品購入できないとき' do
+        it 'tokenが空白(カード認証ができない)場合は購入できない' do
+          @order_address.token = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Token can't be blank")
+        end
+        it '郵便番号が空白だと購入できない' do
+          @order_address.post_code = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Post code can't be blank")
+        end
+        it '郵便番号にハイフンがない場合は購入できない' do
+          @order_address.post_code = '1234567'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Post code is invalid. Enter it as follows (e.g. 123-4567)")
+        end
+        it '郵便番号が「3桁ハイフン4桁」でない場合は購入できない' do
+          @order_address.post_code = '1234-567'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Post code is invalid. Enter it as follows (e.g. 123-4567)")
+        end
+        it '郵便番号が半角数字でない場合は購入できない' do
+          @order_address.post_code = '１２３−４５６７'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Post code is invalid. Enter it as follows (e.g. 123-4567)")
+        end
+        it '都道府県が空白 (prefecture_id = 1)の場合は購入できない' do
+          @order_address.prefecture_id = 1
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+        end
+        it '市区町村が空白の場合は購入できない' do
+          @order_address.city = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("City can't be blank")
+        end
+        it '番地が空白の場合は購入できない' do
+          @order_address.block = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Block can't be blank")
+        end
+        it '電話番号が空白だと登録できない' do
+          @order_address.phone_number = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+        end
+        it '電話番号にハイフンが含まれている場合は登録できない' do
+          @order_address.phone_number = '000-00-0000'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number is invalid. Input only number")
+        end
+        it '電話番号が10桁未満だと購入できない' do
+          @order_address.phone_number = '123456789'
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Phone number is too short")
+        end
+        it 'user情報がない（ログイン状態でない）と登録できない' do
+          @order_address.user_id = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("User can't be blank")
+        end
+        it 'item情報と関連づいていないと登録できない' do
+          @order_address.item_id = ''
+          @order_address.valid?
+          expect(@order_address.errors.full_messages).to include("Item can't be blank")
+        end
+      end
+    end
+end
